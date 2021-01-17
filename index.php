9dc1caf7c0f2d096c4df9369f420690c43026920
KgothatsoN <kg.otha@hotmail.com>
2021-01-17 12:20:18 +0000

    Added bootstrap input field and fontawesome cnd

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tracker</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/b3f24da991.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+        <main>
+            <div class="container text-center">
+                <h1 class="py-4 bg-dark text-light rounded"><i class="fa fa-money"></i> Expense Tracker</h1>
+
+                <div class="d-flex justify-content-center">
+                    <form action="" method="post" class="w-50">
+                        <div class="py-2">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text bg-warning" id="basic-addon1"><i class="fa fa-id-badge" aria-hidden="true"></i></span>
+                                <input type="text" autocomplete="off" class="form-control" placeholder="ID" aria-label="id" aria-describedby="basic-addon1">
+                            </div>    
+                        </div>
+                        <div class="pt-2">
+                                
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+
+    </body>
+</html>
